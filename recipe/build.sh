093dc257e4e4a260cd056cc49cf4ea1c41ef4df3
David Brochart <david.brochart@gmail.com>
2020-02-11 14:20:31 +0100

    Fix export

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ if [ ! -d "${PREFIX}/include/python${HOST_PY_VER}" ]; then
 fi
 
 if [ `uname` == Darwin ]; then
-    setenv CLANG "${CLANG} --trace-includes "
+    export CLANG="${CLANG} --trace-includes "
     PY_LIB="libpython${HOST_PY_VER}.dylib"
     sed -i '' 's/${PYTHON_LIBRARY}//g' $SRC_DIR/src/chrono_python/CMakeLists.txt
     sed -i '' 's/find_package(AVX)//g' $SRC_DIR/src/CMakeLists.txt
