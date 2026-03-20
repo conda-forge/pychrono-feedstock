7b07598ea95d086fb96bd2c91cc669ef0651f9f2
Jacob Oursland <jacob.oursland@gmail.com>
2026-03-20 15:50:58 -0700

    Fix Windows build: use native .bat scripts instead of MSYS2 wrappers
    The MSYS2 bash wrapper approach caused %BUILD_PREFIX% environment
    variable expansion failures when Ninja spawned cmd.exe to run the
    MSVC linker (vs_link_dll). Replaced with native .bat build scripts
    that use cmake directly in the Windows CMD environment.
    
    Also fixed the Windows test command to use native CMD 'if not exist'
    instead of Unix 'test -f'.

--- recipe/build_pychrono.bat
+++ recipe/build_pychrono.bat
@@ -1,9 +1,29 @@
 @echo on
-set MSYSTEM=MINGW%ARCH%
-set MSYS2_PATH_TYPE=inherit
-set CHERE_INVOKING=1
-FOR /F "delims=" %%i in ('cygpath.exe -u "%PREFIX%/Library"') DO set "PREFIX=%%i"
-FOR /F "delims=" %%i in ('cygpath.exe -u "%SRC_DIR%"') DO set "SRC_DIR=%%i"
-copy "%RECIPE_DIR%\build_pychrono.sh" .
-bash -lce "%SRC_DIR%/build_pychrono.sh"
+
+cmake %CMAKE_ARGS% ^
+      -G Ninja ^
+      -B build ^
+      -DBUILD_DEMOS:BOOL=OFF ^
+      -DCH_CONDA_INSTALL:BOOL=ON ^
+      -DCH_INSTALL_PYTHON_PACKAGE="lib/python%PY_VER%/site-packages" ^
+      -DCH_PYCHRONO_DATA_PATH:PATH=data ^
+      -DCH_PYCHRONO_SHADER_PATH:PATH=shader ^
+      -DENABLE_MODULE_CASCADE:BOOL=OFF ^
+      -DENABLE_MODULE_FSI:BOOL=ON ^
+      -DENABLE_MODULE_IRRLICHT:BOOL=OFF ^
+      -DENABLE_MODULE_MATLAB:BOOL=OFF ^
+      -DENABLE_MODULE_OPENGL:BOOL=OFF ^
+      -DENABLE_MODULE_POSTPROCESS:BOOL=OFF ^
+      -DENABLE_MODULE_PYTHON:BOOL=ON ^
+      -DENABLE_MODULE_ROS:BOOL=OFF ^
+      -DENABLE_MODULE_VEHICLE:BOOL=OFF ^
+      -DENABLE_OPENMP:BOOL=ON ^
+      -DUSE_SIMD=OFF ^
+      .
+if errorlevel 1 exit 1
+
+cmake --build build -j%CPU_COUNT%
+if errorlevel 1 exit 1
+
+cmake --install build
 if errorlevel 1 exit 1
